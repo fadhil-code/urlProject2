e045d960b5a9bc89e22bdda0ab281fd1c87f1f11
Fadhil <david.fadhil11@gmail.com>
2021-03-15 21:29:04 +0300

    URL project NLP examples

--- main.py
+++ main.py
@@ -22,10 +22,9 @@ from nltk import word_tokenize
 sentences = sent_tokenize(text)
 
 #How many sentences are there? :
-print (len(sentences))
+print ( len(sentences))
 
 #Print the sentences :
-#print(sentences)
 print(sentences)
 
 #Tokenize the text with words :
@@ -37,3 +36,42 @@ print("\n")
 
 #Print words :
 print (words)
+
+from nltk.probability import FreqDist
+
+#Find the frequency :
+fdist = FreqDist(words)
+
+#Print 10 most common words :
+fdist.most_common(10)
+
+#Plot the graph for fdist :
+import matplotlib.pyplot as plt
+
+fdist.plot(10)
+
+#Empty list to store words:
+words_no_punc = []
+
+#Removing punctuation marks :
+for w in words:
+    if w.isalpha():
+        words_no_punc.append(w.lower())
+
+#Print the words without punctution marks :
+print (words_no_punc)
+
+print ("\n")
+
+#Length :
+print (len(words_no_punc))
+
+#Frequency distribution :
+fdist = FreqDist(words_no_punc)
+
+fdist.most_common(10)
+
+
+#Plot the most common words on grpah:
+
+fdist.plot(10)
